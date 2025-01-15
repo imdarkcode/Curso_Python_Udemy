5f8f418c0dd0da81bf17dde808ac3550fdc6a774
Otavio <otavio.sudano@email.com>
2025-01-15 06:20:54 -0300

    Adicionando aula 55

---
+++ aula_55.py
@@ -0,0 +1,20 @@
+# Split, Strip e Join
+
+# A função split() divide as palavras de uma string com base no parâmetro que é passado
+
+string = "Uma frase qualquer"
+palavras = string.split()
+print(palavras)
+
+# A função strip() remove os espaços antes e depois da string
+
+string = " Uma frase qualquer "
+print(string.strip())
+
+# A função join() cria um separador entre cada índice de uma string ou lista
+
+string = "abcde"
+print("-".join(string))
+
+
+
