87ae9de2d25ead838843d5bfd396922422fd64f3
Otavio <otavio.sudano@email.com>
2025-01-15 06:48:35 -0300

    Adicionando aula 57

---
+++ aula_57.py
@@ -0,0 +1,7 @@
+# Comandos do interpretador do Python
+
+# py --version (mostrar a versão do Python instalada)
+# py --help (mostrar uma lista de comandos)
+# py nome_do_arquivo.py (executar um arquivo)
+# py -mod (executar uma biblioteca)
+# py -c (executar um comando de Python)
