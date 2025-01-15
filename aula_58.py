3d0b2d1dc5c962ea14c9e528473c73a43cee9085
Otavio <otavio.sudano@email.com>
2025-01-15 06:57:03 -0300

    Adicionando aula 58

---
+++ aula_58.py
@@ -0,0 +1,12 @@
+# Desempacotamento utilizando funções
+
+lista = ["Carlos", "Eloisa", "Livia", "Renato"]
+
+# Uma maneira de desempacotar os valores de uma lista é usando o for
+
+for nome in lista:
+    print(nome)
+
+# Outra maneira é utilizando o nome da lista com o símbolo de * antes
+
+print(*lista)
