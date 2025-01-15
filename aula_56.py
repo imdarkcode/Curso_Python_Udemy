1dd91cb6acf65f5983215078eccf8109a86d0895
Otavio <otavio.sudano@email.com>
2025-01-15 06:21:02 -0300

    Adicionando aula 56

---
+++ aula_56.py
@@ -0,0 +1,11 @@
+# Matrizes
+
+letras = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"]
+]
+
+# Para pegar o valor específico é só colocar os índices na ordem de hierarquia
+
+print(letras[2][1])
