684d94211e29f7a9a43d80e3a7437e44f9f0ea2b
Otávio Sudano <otavio.sudano13@gmail.com>
2025-01-21 14:55:09 -0300

    Adicionando aula 68

---
+++ aula_68.py
@@ -0,0 +1,9 @@
+"""
+Função sum(): é uma função do Python que calcula a soma dos argumnetos
+igual a função criada na aula anterior, essa função por padrão só aceita
+dois argumentos, mas para isso é só colocar os argumnetos dentro de uma 
+lista ou tupla
+"""
+
+print(sum((45, 74, 12, 45, 6, 45)))
+
