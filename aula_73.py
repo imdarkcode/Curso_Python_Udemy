d797f1b8cd9e8e8d4e92d5f25f99792ec1c0e7e8
Otávio Sudano <otavio.sudano13@gmail.com>
2025-01-23 09:24:37 -0300

    Adicionando aula 73

---
+++ aula_73.py
@@ -0,0 +1,17 @@
+"""
+Exercício
+
+Crie funções que duplicam, triplicam e quadruplicam o
+número recebido como parâmetro
+"""
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(54))
