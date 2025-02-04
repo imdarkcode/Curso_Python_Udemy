a35f0db43ebee546854a882ff47382ea0d0cff86
Otávio Sudano <otavio.sudano13@gmail.com>
2025-02-04 15:24:41 -0300

    Adicionando aula 78

---
+++ aula_78.py
@@ -0,0 +1,16 @@
+"""
+Sets: Conjuntos em Python
+Ele é um conjunto de valores que não possui index e não possuem uma ordem certa
+"""
+
+# Criando um tipo de dado set
+
+conjunto_1 = set("Otávio")
+conjunto_2 = {"O", "t", "á", "v", "i", "o"}
+print(conjunto_1)
+print(conjunto_2)
+
+# Por padrão eles removem valores duplicados
+
+conjunto_3 = {1, 2, 3, 3, 3, 4, 1}
+print(conjunto_3)
